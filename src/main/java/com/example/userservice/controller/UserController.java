f5b736fa172d6265d9ace93f542f7f3d0ccb2961
이영선 <yslee@gridone.co.kr>
2023-04-12 14:25:30 +0900

    micrometer

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
@@ -33,6 +34,7 @@ public class UserController {
 //    Greeting greeting;
 
     @GetMapping("/health_check")
+    @Timed(value = "user.status", longTask = true)
     public String status() {
         return String.format("It's Working in User Service"
                 + ", port(local.server.port) = " + env.getProperty("local.server.port")
@@ -43,6 +45,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value = "user.welcome", longTask = true)
     public String welcome() {
 //        return greeting.getMessage();
         return env.getProperty("greeting.message");
